80090f9c31d9d160cffecfcdc5e494a3072e03e2
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-03-13 21:19:01 +0200

    cleanup

--- assignment2/gameOfLife.c
+++ assignment2/gameOfLife.c
@@ -1,43 +1,36 @@
 #include "gameOfLife.h"
 
-int main(int argc,char** argv){
+int main(int argc,char** argv) {
 
     int rows, columns, generations = atoi(argv[2]);
     char** array = read_file(argv[1], &rows, &columns);
     FILE* output = fopen(argv[3],"w");
-    omp_set_num_threads(atoi(argv[4]));
+    numOfThreads = atoi(argv[4]);
 
-    game_of_life_serial(array, columns, rows, generations);
-
-    FILE* outputSerial = fopen("outpuSerial.txt","w");
+    game_of_life_parallel(array, columns, rows, generations);
 
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < columns; j++){
-            fprintf(outputSerial, "|%c", array[i][j]);
+            fprintf(output, "|%c", array[i][j]);
         }
-         fprintf(outputSerial, "|\n");
+        fprintf(output, "|\n");
     }
-
-    // game_of_life_parallel(array, columns, rows, generations);
-
-    // for(int i = 0; i < rows; i++){
-    //     for(int j = 0; j < columns; j++){
-    //         fprintf(output, "|%c", array[i][j]);
-    //     }
-    //     fprintf(output, "|\n");
-    // }
-    // printf("test");
 
 }
 
-void game_of_life_parallel(char **array, int columns, int rows, int generations){
-    char **newArray = make_array(columns, rows);
+void game_of_life_parallel(char **array, int columns, int rows, int generations) {
 
+    char** newArray = make_array(columns, rows);
     int numNeighbors,currentGen = 0, i = 0 , j = 0;
-    #pragma omp for
-    for(int currentGen = 0; currentGen < generations; currentGen++){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < columns; j++){
+
+    omp_set_dynamic(0);
+    omp_set_num_threads(numOfThreads);
+    
+    for(currentGen = 0; currentGen < generations; currentGen++){
+
+        #pragma omp parallel for private(i, j, numNeighbors)
+        for(i = 0; i < rows; i++){
+            for(j = 0; j < columns; j++){
                 numNeighbors = count_living_neighbors(i, j, array, rows, columns);
                 if( numNeighbors == 3 ) newArray[i][j] = alive;
                 else if( numNeighbors == 2 && array[i][j] == alive) newArray[i][j] = alive;
@@ -45,34 +38,15 @@ void game_of_life_parallel(char **array, int columns, int rows, int generations)
             }
         }
         
-        #pragma omp critical
-        {
-            copy_array(array,newArray,columns);
-        }
-    }
-}
-
-void game_of_life_serial(char** array, int columns, int rows, int generations){
-
-    char** newArray = make_array(columns, rows);
-
-    int numNeighbors;
-    for(int currentGen = 0; currentGen < generations; currentGen++){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < columns; j++){
-                numNeighbors = count_living_neighbors(i, j, array, rows, columns);
-                if( numNeighbors == 3 ) newArray[i][j] = alive;
-                else if( numNeighbors == 2 && array[i][j] == alive) newArray[i][j] = alive;
-                else newArray[i][j] = dead;
-            }
-        }
-        copy_array(array,newArray,columns);
+        copy_array_parallel(array, newArray, rows, columns);
     }
 
 }
 
 int count_living_neighbors(int row, int col, char** array, int numOfRows, int numOfCols) {
+
     int count = 0;
+
     if(col + 1 < numOfCols) count += (array[row][col+1] == alive);
     if(col - 1 >= 0) count += (array[row][col-1] == alive);
     if(row - 1 >= 0) {
@@ -85,27 +59,39 @@ int count_living_neighbors(int row, int col, char** array, int numOfRows, int nu
         if(col + 1 < numOfCols) count+= (array[row+1][col+1] == alive);
         if(col - 1 >= 0) count += (array[row+1][col-1] == alive);
     }
+
     return count;
+    
 }
 
-char** make_array(int columns, int rows){
+char** make_array(int columns, int rows) {
+
     char** newArray = (char**)malloc(rows * sizeof(char*));
     check_memory_ptr(newArray);
+
     for(int i = 0; i < rows; i++){
         newArray[i] = (char*) malloc(columns * sizeof(char));
         check_memory_ptr(newArray[i]);
     }
+
     return newArray;
+
 }
 
-void copy_array(char** dst, char** src, int size){
-    for(int i = 0; i < size; i++){
-        strcpy(dst[i],src[i]);
+void copy_array_parallel(char** dst, char** src, int rows, int columns) {
+
+    int i = 0, j = 0;
+
+    #pragma omp parallel for private (i, j)
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < columns; j++)
+            dst[i][j] = src[i][j];
     }
+    
 }
 
 
-char** read_file(const char* fileName, int* _rows, int* _columns){
+char** read_file(const char* fileName, int* _rows, int* _columns) {
 
     FILE* file = fopen(fileName,"r");
     check_file_ptr(file,fileName);
@@ -142,18 +128,23 @@ char** read_file(const char* fileName, int* _rows, int* _columns){
     *_rows = rows;
     *_columns = columns;
     return array;
+
 }
 
-void check_file_ptr(FILE* filePtr, const char* fileName){
+void check_file_ptr(FILE* filePtr, const char* fileName) {
+
     if(filePtr == NULL) { 
         printf("Could not open file %s",fileName); 
         exit(EXIT_FAILURE); 
     }
+
 }
 
-void check_memory_ptr(void* ptr){
+void check_memory_ptr(void* ptr) {
+
     if(ptr == NULL) {
         printf("Could not allocate memory");
         exit(EXIT_FAILURE);
     }
+
 }
--- assignment2/gameOfLife.h
+++ assignment2/gameOfLife.h
@@ -11,9 +11,8 @@ int numOfThreads;
 
 char** read_file(const char* filename, int* rows, int* columns);
 void game_of_life_parallel(char** array, int columns, int rows, int generations);
-void game_of_life_serial(char** array, int columns, int rows, int generations);
 int count_living_neighbors(int row, int col, char** array, int numOfRows, int numOfCols);
 char** make_array(int columns, int rows);
-void copy_array_parallel(char** dst, char** src, int size);
+void copy_array_parallel(char** dst, char** src, int rows, int columns);
 void check_file_ptr(FILE* filePtr, const char* fileName);
 void check_memory_ptr(void* ptr);
