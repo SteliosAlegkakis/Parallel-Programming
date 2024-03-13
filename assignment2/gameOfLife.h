3a5ef20b624bf2c02c8f9418937d79402de67b56
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-03-13 20:35:53 +0200

    fixed parallel implementation

--- assignment2/gameOfLife.h
+++ assignment2/gameOfLife.h
@@ -7,11 +7,13 @@
 #define bar '|'
 #define new_line '\n'
 
+int numOfThreads;
+
 char** read_file(const char* filename, int* rows, int* columns);
 void game_of_life_parallel(char** array, int columns, int rows, int generations);
 void game_of_life_serial(char** array, int columns, int rows, int generations);
 int count_living_neighbors(int row, int col, char** array, int numOfRows, int numOfCols);
 char** make_array(int columns, int rows);
-void copy_array(char** dst, char** src, int size);
+void copy_array_parallel(char** dst, char** src, int size);
 void check_file_ptr(FILE* filePtr, const char* fileName);
 void check_memory_ptr(void* ptr);
